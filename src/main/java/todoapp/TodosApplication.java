daecb98fe1a75e7c189ccd6ed31793c2d2a0809d
admin <gkaeogus7@gmail.com>
2023-08-22 17:13:10 +0900

    20230822

--- src/main/java/todoapp/TodosApplication.java
+++ src/main/java/todoapp/TodosApplication.java
@@ -3,6 +3,7 @@ package todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import todoapp.web.WebMvcConfiguration;
 
 @SpringBootApplication
@@ -10,10 +11,5 @@ public class TodosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfiguration todoappWebConfiguration() {
-		return new WebMvcConfiguration();
 	}
 }
--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,6 +1,9 @@
 package todoapp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
@@ -13,10 +16,31 @@ import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public WebMvcConfiguration() {
+        logger.debug("스프링 MVC 설정자가 생성됩니다.");
+    }
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         // registry.enableContentNegotiation();
         // 위와 같이 직접 설정하면, 스프링부트가 구성한 ContentNegotiatingViewResolver 전략이 무시된다.
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 리소스 핸들러를 등록, 정적 자원을 처리할 수 있습니다.
+
+        // https://localhost:8080/assets/css/todos.css
+
+        // 서블릿 컨텍스트 경로에서 정적 자원 제공
+        registry.addResourceHandler("/assets/**").addResourceLocations("assets/");
+
+        // 파일 경로에서 정적 자원 제공
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("file:/Users/arawn/springrunner/workspace/todos/files/assets");
+
     }
 
     /**
