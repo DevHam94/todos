d6ab4e0f79302c52a17e65c5b4af9fef93e8b9bb
admin <gkaeogus7@gmail.com>
2023-08-22 16:19:22 +0900

    20230822

--- src/main/java/todoapp/TodosApplication.java
+++ src/main/java/todoapp/TodosApplication.java
@@ -2,12 +2,18 @@ package todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import todoapp.web.WebMvcConfiguration;
 
 @SpringBootApplication
 public class TodosApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TodosApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TodosApplication.class, args);
+	}
 
+	@Bean
+	public WebMvcConfiguration todoappWebConfiguration() {
+		return new WebMvcConfiguration();
+	}
 }
