049ffc06052749b7b6673ff4b3abd6da1b691341
admin <gkaeogus7@gmail.com>
2023-08-23 18:21:51 +0900

    20230823

--- src/main/java/todoapp/web/TodoRestController.java
+++ src/main/java/todoapp/web/TodoRestController.java
@@ -3,17 +3,26 @@ package todoapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import todoapp.core.todos.application.TodoFinder;
 import todoapp.core.todos.domain.Todo;
 
 import java.util.Collections;
 import java.util.List;
 
+@RestController
 @Controller
 public class TodoRestController {
+
+    private final TodoFinder finder;
+
+    public TodoRestController(TodoFinder finder) {
+        this.finder = finder;
+    }
 
     @RequestMapping("/api/todos")
     @ResponseBody
     public List<Todo> list() {
-        return Collections.emptyList();
+        return finder.getAll();
     }
 }
