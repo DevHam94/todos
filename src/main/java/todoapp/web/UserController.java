8bbb7557c8d6b4d77c75f7e1890f1a7e040e7a5c
admin <gkaeogus7@gmail.com>
2023-09-04 12:30:26 +0900

    20230904

--- src/main/java/todoapp/web/UserController.java
+++ src/main/java/todoapp/web/UserController.java
@@ -1,13 +1,20 @@
 package todoapp.web;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import todoapp.core.user.domain.ProfilePicture;
 import todoapp.core.user.domain.ProfilePictureStorage;
 import todoapp.security.UserSession;
 
 import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,17 +23,40 @@ import java.nio.file.Paths;
 @Controller
 public class UserController {
 
-    private final ProfilePictureStorage profilePictureStorage;
-
-    public UserController(ProfilePictureStorage profilePictureStorage) {
-        this.profilePictureStorage = profilePictureStorage;
-    }
+//    private final ProfilePictureStorage profilePictureStorage;
+//
+//    public UserController(ProfilePictureStorage profilePictureStorage) {
+//        this.profilePictureStorage = profilePictureStorage;
+//    }
 
     @RequestMapping("/user/profile-picture")
     @RolesAllowed("ROLE_USER")
-    public @ResponseBody Resource profilePicture(UserSession userSession) throws IOException {
+    public ProfilePicture profilePicture(UserSession userSession) throws IOException {
 //        Path profilePicturePath = Paths.get(userSession.getUser().getProfilePicture().getUri());
-        return profilePictureStorage.load(userSession.getUser().getProfilePicture().getUri());
+//        return profilePictureStorage.load(userSession.getUser().getProfilePicture().getUri());
 //        return Files.readAllBytes(profilePicturePath);
+        return userSession.getUser().getProfilePicture();
+    }
+
+    public static class ProfilePictureReturnValueHandler implements HandlerMethodReturnValueHandler {
+
+        private final ProfilePictureStorage profilePictureStorage;
+
+        public ProfilePictureReturnValueHandler(ProfilePictureStorage profilePictureStorage) {
+            this.profilePictureStorage = profilePictureStorage;
+        }
+
+        @Override
+        public boolean supportsReturnType(MethodParameter returnType) {
+            return ProfilePicture.class.isAssignableFrom(returnType.getParameterType());
+        }
+
+        @Override
+        public void handleReturnValue(Object returnValue, MethodParameter returnType,
+                                      ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+            HttpServletResponse response = webRequest.getNativeResponse(HttpServletResponse.class);
+            Resource profilePicture = profilePictureStorage.load(((ProfilePicture) returnValue).getUri());
+            profilePicture.getInputStream().transferTo(response.getOutputStream());
+        }
     }
 }
--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -23,6 +24,7 @@ import todoapp.commons.web.servlet.ExecutionTimeHandlerInterceptor;
 import todoapp.commons.web.servlet.LoggingHandlerInterceptor;
 import todoapp.commons.web.view.CommaSeparatedValuesView;
 import todoapp.core.todos.domain.Todo;
+import todoapp.core.user.domain.ProfilePictureStorage;
 import todoapp.security.UserSession;
 import todoapp.security.UserSessionRepository;
 import todoapp.security.web.servlet.RolesVerifyHandlerInterceptor;
@@ -46,6 +48,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
     private UserSessionRepository userSessionRepository;
 
+    @Autowired
+    private ProfilePictureStorage profilePictureStorage;
+
     public WebMvcConfiguration() {
         logger.debug("스프링 MVC 설정자가 생성됩니다.");
     }
@@ -53,6 +58,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new UserSessionHandlerMethodArgumentResolver(userSessionRepository));
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
+        handlers.add(new UserController.ProfilePictureReturnValueHandler(profilePictureStorage));
     }
 
     @Override
