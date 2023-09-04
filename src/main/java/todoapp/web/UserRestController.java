554ac2173e9f9bd76135db78064fe1e5cd5d75c6
admin <gkaeogus7@gmail.com>
2023-09-04 10:29:52 +0900

    20230904

--- src/main/java/todoapp/web/UserRestController.java
+++ src/main/java/todoapp/web/UserRestController.java
@@ -1,12 +1,17 @@
 package todoapp.web;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.multipart.MultipartFile;
+import todoapp.core.user.application.ProfilePictureChanger;
+import todoapp.core.user.domain.ProfilePicture;
 import todoapp.core.user.domain.User;
 import todoapp.security.UserSession;
 import todoapp.security.UserSessionRepository;
@@ -14,9 +19,13 @@ import todoapp.web.model.UserProfile;
 
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 @RestController
+@RolesAllowed("ROLE_USER")
 public class UserRestController {
 /*    private final UserSessionRepository userSessionRepository;
 
@@ -24,8 +33,15 @@ public class UserRestController {
         this.userSessionRepository = userSessionRepository;
     }*/
 
+    private final ProfilePictureChanger profilePictureChanger;
+    private final UserSessionRepository userSessionRepository;
+
+    public UserRestController(ProfilePictureChanger profilePictureChanger, UserSessionRepository userSessionRepository) {
+        this.profilePictureChanger = profilePictureChanger;
+        this.userSessionRepository = userSessionRepository;
+    }
+
     @GetMapping("/api/user/profile")
-    @RolesAllowed("ROLE_USER")
     public ResponseEntity<UserProfile> userProfile(UserSession userSession) {
         //User user = (User) session.getAttribute("user");
         //UserSession userSession = userSessionRepository.get();
@@ -36,5 +52,24 @@ public class UserRestController {
 //        }
 //        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         return ResponseEntity.ok(new UserProfile(userSession.getUser()));
+    }
+
+    @PostMapping("/api/user/profile-picture")
+    public UserProfile changeProfilePicture(MultipartFile profilePicture, UserSession userSession) throws IOException {
+
+        // 1. 업로드된 프로필 이미지 파일 저장하기
+        Path basePath = Paths.get("./files/user-profile-picture");
+        if(!basePath.toFile().exists()) {
+            basePath.toFile().mkdirs();
+        }
+        Path profilePicturePath = basePath.resolve(profilePicture.getOriginalFilename());
+        profilePicture.transferTo(profilePicturePath);
+
+        // 2. 프로필 이미지 변경 후 세션을 갱신하기
+        User updatedUser = profilePictureChanger.change(userSession.getName(), new ProfilePicture(profilePicturePath.toUri()));
+        userSessionRepository.set(new UserSession(updatedUser));
+
+
+        return new UserProfile(updatedUser);
     }
 }
